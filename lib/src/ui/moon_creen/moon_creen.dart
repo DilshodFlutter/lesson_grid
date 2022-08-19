47f407c1193e6d2dd9a2946cc37c586dd9d69013
dilshod_egamberdiyev <www.emacho900@gmail.com>
2022-08-19 11:45:52 +0500

    okm

---
+++ lib/src/ui/moon_creen/moon_creen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoonScreen extends StatefulWidget {
+  const MoonScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MoonScreen> createState() => _MoonScreenState();
+}
+
+class _MoonScreenState extends State<MoonScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
